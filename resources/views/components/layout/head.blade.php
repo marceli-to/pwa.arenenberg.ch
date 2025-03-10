efb843eaad73ee16d01d10787ba15c5f323ee346
Marcel Stadelmann <m@marceli.to>
2025-03-10 15:57:50 +0100

    wip

--- resources/views/components/layout/head.blade.php
+++ resources/views/components/layout/head.blade.php
@@ -5,12 +5,12 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>{{ env('APP_NAME') }}</title>
-<meta name="description" content="{{ env('APP_DESCRIPTION') }}">
-<meta property="og:title" content="{{ env('APP_NAME') }}">
-<meta property="og:description" content="{{ env('APP_DESCRIPTION') }}">
+<title>{{ __(env('APP_NAME')) }}</title>
+<meta name="description" content="{{ __(env('APP_DESCRIPTION')) }}">
+<meta property="og:title" content="{{ __(env('APP_NAME')) }}">
+<meta property="og:description" content="{{ __(env('APP_DESCRIPTION')) }}">
 <meta property="og:url" content="{{ url()->current()}}">
-<meta property="og:site_name" content="{{ env('APP_NAME') }}">
+<meta property="og:site_name" content="{{ __(env('APP_NAME')) }}">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
 <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -18,7 +18,7 @@
 <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 <link rel="shortcut icon" href="/favicon.ico" />
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-<meta name="apple-mobile-web-app-title" content="{{ env('APP_NAME') }}" />
+<meta name="apple-mobile-web-app-title" content="{{ __(env('APP_NAME')) }}" />
 <link rel="manifest" href="/site.webmanifest" />
 @vite('resources/css/app.css')
 </head>
