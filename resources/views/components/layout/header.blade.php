caae2c5de2b376a571c0413f37f5765dd15087ed
Marcel Stadelmann <m@marceli.to>
2025-03-10 14:51:05 +0100

    wip

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,6 +1,6 @@
 <header>
   <x-layout.inner>
-    <div class="w-full flex justify-between">
+    <div class="w-full flex flex-col justify-between">
       @if (Route::is('auth.*'))
         <h1>DASHBOARD</h1>
       @else
@@ -8,6 +8,14 @@
           {{ __('Kaiser, Kühe und Kultur') }}
         </h1>
       @endif
+      <nav>
+        <ul>
+          <li>
+            <a href="{{ localized_route('page.locations') }}">
+              {{ __('Standorte') }}
+            </a>
+          </li>
+        </ul>
     </div>
   </x-layout.inner>
 </header>
--- resources/views/pages/locations.blade.php
+++ resources/views/pages/locations.blade.php
@@ -1,5 +1,5 @@
 <x-layout.guest>
   @section('content')
-    <h1>{{ __('Standorte') }}</h1>
+    <h1 class="text-crimson text-lg leading-none">{{ __('Standorte') }}</h1>
   @endsection
   </x-layout.guest>
