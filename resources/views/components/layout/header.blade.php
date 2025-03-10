71f37672bba3c4a595cbc013a04cb21f75bf4711
Marcel Stadelmann <m@marceli.to>
2025-03-10 14:53:16 +0100

    wip

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -8,6 +8,27 @@
           {{ __('Kaiser, Kühe und Kultur') }}
         </h1>
       @endif
+
+      <nav {{ $attributes->merge(['class' => '']) }}>
+        <ul class="flex gap-x-24 lg:gap-x-20">
+          <li>
+            <a 
+              href="{{ Request::routeIs('page.home') ? '/' : current_route('fr') }}"
+              class="{{ app()->getLocale() === 'fr' ? 'is-active' : 'hover:text-coral transition-colors' }}">fr</a>
+          </li>
+          <li>
+            <a 
+              href="{{ Request::routeIs('page.home') ? '/de' : current_route('de') }}"
+              class="{{ app()->getLocale() === 'de' ? 'is-active' : 'hover:text-coral transition-colors' }}">de</a>
+          </li>
+          <li>
+            <a 
+              href="{{ Request::routeIs('page.home') ? '/en' : current_route('en') }}"
+              class="{{ app()->getLocale() === 'en' ? 'is-active' : 'hover:text-coral transition-colors' }}">en</a>
+          </li>
+        </ul>
+      </nav>
+
       <nav>
         <ul>
           <li>
