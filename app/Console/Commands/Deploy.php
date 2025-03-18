aa75381c372a7562fbbb2a854355af0ee7dcf1c5
Marcel Stadelmann <m@marceli.to>
2025-03-18 18:41:01 +0100

    wip

--- app/Console/Commands/Deploy.php
+++ app/Console/Commands/Deploy.php
@@ -49,29 +49,23 @@ class Deploy extends Command
         // Update all links within <nav></nav> to add /index.html at the end
         $htmlContent = file_get_contents($filePath);
         
-        // Find all nav elements and process each one
-        preg_match_all('/<nav[^>]*>(.*?)<\/nav>/s', $htmlContent, $navMatches);
-        
-        if (!empty($navMatches[0])) {
-            foreach ($navMatches[0] as $navContent) {
-                // Find all links in nav that don't already end with /index.html
-                preg_match_all('/<a href="([^"]+)(?<!\/index\.html)"/', $navContent, $matches);
-                
-                if (count($matches[1]) > 0) {
-                    $newNavContent = $navContent;
-                    foreach ($matches[1] as $link) {
-                        // Add /index.html to the link if it doesn't already have it
-                        $newLink = rtrim($link, '/') . '/index.html';
-                        $newNavContent = str_replace('href="' . $link . '"', 'href="' . $newLink . '"', $newNavContent);
-                    }
-                    // Replace this specific nav content with the new one
-                    $htmlContent = str_replace($navContent, $newNavContent, $htmlContent);
-                }
+        // Find all links in the HTML content that don't already end with /index.html
+        preg_match_all('/<a href="([^"]+)(?<!\/index\.html)"/', $htmlContent, $matches);
+
+        if (count($matches[1]) > 0) {
+            $originalHtmlContent = $htmlContent;
+            
+            foreach ($matches[1] as $link) {
+                // Add /index.html to the link if it doesn't already have it
+                $newLink = rtrim($link, '/') . '/index.html';
+                $htmlContent = str_replace('href="' . $link . '"', 'href="' . $newLink . '"', $htmlContent);
             }
             
             // Only write to file and log if changes were made
-            file_put_contents($filePath, $htmlContent);
-            $this->info("Updated links in <nav></nav> for file: " . $filePath);
+            if ($originalHtmlContent !== $htmlContent) {
+                file_put_contents($filePath, $htmlContent);
+                $this->info("Updated links in file: " . $filePath);
+            }
         }
       }
     }
