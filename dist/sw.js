67ced77b3f82bd8e01ec9e23e03c089837c919b4
Marcel Stadelmann <m@marceli.to>
2025-03-18 20:13:20 +0100

    wip

--- dist/sw.js
+++ dist/sw.js
@@ -50,7 +50,7 @@ const ASSETS = [
 	'/web-app-manifest-512x512.png',
 	'/zugang/index.html',
 ];
-const CACHE_NAME = `arenenberg-assets-v21`;
+const CACHE_NAME = `arenenberg-assets-v22`;
 
 // Pre-cache all assets during installation
 self.addEventListener('install', (event) => {
