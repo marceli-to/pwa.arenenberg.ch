6d4577c65eaf9da95647d17305450fb87f2289cc
Marcel Stadelmann <m@marceli.to>
2025-06-03 16:37:22 +0200

    wip

--- resources/views/pages/locations/map.blade.php
+++ resources/views/pages/locations/map.blade.php
@@ -4,13 +4,12 @@
     class="w-full mt-50 flex flex-col justify-between">
     <x-locations.map />
   </div>
-  
+
   <div class="fixed bottom-25 w-full left-1/2 -translate-x-1/2">
     <x-buttons.primary 
       route="{{ localized_route('page.locations.list') }}"
       type="link"
-      label="{{ __('Liste') }}"
-      class="absolute bottom-36 left-1/2 -translate-x-1/2">
+      label="{{ __('Liste') }}">
     </x-buttons.primary>
   </div>
   @endsection
