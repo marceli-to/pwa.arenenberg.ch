3e9476c1fef7a79e2e84fc98e6529346f423bafa
Marcel Stadelmann <m@marceli.to>
2025-06-03 16:20:23 +0200

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,10 +1,10 @@
 <x-layout.app>
 @section('content')
-<div class="h-[calc(100dvh_-_165px)]">
-  <figure class="h-[calc(100dvh_-_360px)] mt-25">
-    <img src="/img/visual-home.png" alt="" width="698" height="956" class="w-full h-full object-contain">
+<div>
+  <figure class="mt-25">
+    <img src="/img/visual-home.png" alt="" width="698" height="956" class="w-full h-auto pb-180">
   </figure>
-  <div class="flex flex-col gap-y-25 h-155 mt-30">
+  <div class="fixed bottom-25 left-1/2 -translate-x-1/2 flex flex-col gap-y-25 h-155 pt-25">
     <div class="flex justify-center gap-45 max-w-[290px] mx-auto">
       <x-buttons.language route="{{ current_route('de') }}" label="D" active="{{ locale() === 'de' }}" language="de" />
       <x-buttons.language route="{{ current_route('fr') }}" label="F" active="{{ locale() === 'fr' }}" language="fr" />
