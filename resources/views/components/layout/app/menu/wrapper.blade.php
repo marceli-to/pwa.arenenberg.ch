a29f39ff82afe2becf3fbca3dbb4fbc8740b5d12
Marcel Stadelmann <m@marceli.to>
2025-06-03 15:47:53 +0200

    wip

--- resources/views/components/layout/app/menu/wrapper.blade.php
+++ resources/views/components/layout/app/menu/wrapper.blade.php
@@ -19,7 +19,44 @@
       title="{{ __('Impressum') }}" 
       id="impressum" 
       contentClasses="block w-full !py-20">
-      <p>Impressum content goes here.</p>
+
+      <p>
+        {{ __('Redaktion: Kanton Thurgau, Betrieb Arenenberg') }}<br>
+        {{ __('Konzept und Inhaltserarbeitung: imRaum, Baden') }}<br>
+        {{ __('Konzept und Grafik: Bivgrafik, Zürich') }}<br>
+        {{ __('Konzept und Szenografie: Studio DAS, St. Gallen') }}<br>
+        {{ __('Illustrationen: Laura Jurt, Zürich') }}
+      </p>
+      
+      <p>
+        {{ __('Hörreportagen') }}<br>
+        {{ __('Interviews: Annette Fetscherin; Daniel Brogle, Christina Egli, Dominik Gügel, Hansjörg Hauser, Marlis Nölly, Michael Schwarzenberger, Carol Tanner') }}<br>
+        {{ __('Tonaufnahmen: Fruitjuicer, Weinfelden') }}<br>
+        {{ __('Post-Produktion Audio: kellerthurgau, Frauenfeld') }}<br>
+        {{ __('Sprecherinnen und Sprecher F/E : Pascal Holzer (E), Laura Lienhard (F), Ann Malcolm (E), Sophie Richard (F), Suramira Vos (E), Olivier Vuille (F)') }}
+      </p>
+      
+      <p>
+        {{ __('Film') }}<br>
+        {{ __('Konzept und Animation: Papierboot, Luzern') }}<br>
+        {{ __('Sounddesign: Noisy Neighbours, Zürich') }}<br>
+        {{ __('Sprecher Film: Marco Caduff') }}
+      </p>
+      
+      <p>
+        {{ __('Programmierung: Marcel Stadelmann, Winterthur/Zürich') }}<br>
+        {{ __('Übersetzungen: Supertext, Zürich') }}
+      </p>
+      
+      <p>
+        {{ __('Geländevorbereitung/Bau: Kanton Thurgau, Betrieb Arenenberg') }}<br>
+        {{ __('Vorbereitung Exponate: Kanton Thurgau, Departement für Erziehung und Kultur, Amt für Archäologie') }}<br>
+        {{ __('Metallbau: FMT Metallbau, Bettwiesen') }}<br>
+        {{ __('AV-Technik: Crystal Display Electronics, Altendorf') }}<br>
+        {{ __('Digitaldruck: Capa Werbetechnik, Frauenfeld') }}<br>
+        {{ __('Druck Flyer: Fairdruck, Sirnach') }}
+      </p>
+      
     </x-accordion.item>
 
   </x-accordion.wrapper>
